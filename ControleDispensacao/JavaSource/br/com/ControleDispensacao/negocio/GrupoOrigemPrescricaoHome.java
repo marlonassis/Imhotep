557c4a57ef22b30af70bf04b7bf0bb4f7db9b4e5
marlonassis <marlonassis@gmail.com>
2012-02-21 01:04:15 -0300

    Criada a entidade GrupoOrigemPrescricao

---
+++ ControleDispensacao/JavaSource/br/com/ControleDispensacao/negocio/GrupoOrigemPrescricaoHome.java
@@ -0,0 +1,40 @@
+package br.com.ControleDispensacao.negocio;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.ControleDispensacao.entidade.GrupoOrigemPrescricao;
+import br.com.ControleDispensacao.seguranca.Autenticador;
+import br.com.nucleo.PadraoHome;
+
+@ManagedBean(name="grupoOrigemPrescricaoHome")
+@SessionScoped
+public class GrupoOrigemPrescricaoHome extends PadraoHome<GrupoOrigemPrescricao>{
+	
+	/**
+	 * Método que retorna uma lista de GrupoOrigemPrescricao
+	 * @param String sql
+	 * @return Collection GrupoOrigemPrescricao
+	 */
+	public Collection<GrupoOrigemPrescricao> getListaFabricanteAutoComplete(String sql){
+		return super.getBusca("select o from GrupoOrigemPrescricao as o where o.descricao like '%"+sql+"%' ");
+	}
+	
+	@Override
+	public boolean atualizar() {
+		getInstancia().setUsuarioAlteracao(Autenticador.getUsuarioAtual());
+		getInstancia().setDataAlteracao(new Date());
+		return super.atualizar();
+	}
+	
+	@Override
+	public boolean enviar() {
+		getInstancia().setUsuarioInclusao(Autenticador.getUsuarioAtual());
+		getInstancia().setDataInclusao(new Date());
+		return super.enviar();
+	}
+	
+}
