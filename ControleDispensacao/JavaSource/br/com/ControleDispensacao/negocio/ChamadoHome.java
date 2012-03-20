10faba14a6bd33a8c6c83c5801f624feff838f39
marlonassis <marlonassis@gmail.com>
2012-03-20 13:11:15 -0300

    Implementado o envio de e-mail. Ainda incompleto.

--- ControleDispensacao/JavaSource/br/com/ControleDispensacao/negocio/ChamadoHome.java
+++ ControleDispensacao/JavaSource/br/com/ControleDispensacao/negocio/ChamadoHome.java
@@ -5,6 +5,9 @@ import java.util.Date;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
+
 import br.com.ControleDispensacao.entidade.Chamado;
 import br.com.ControleDispensacao.enums.TipoBooleanEnum;
 import br.com.ControleDispensacao.seguranca.Autenticador;
@@ -19,5 +22,39 @@ public class ChamadoHome extends PadraoHome<Chamado>{
 		getInstancia().setDataChamado(new Date());
 		getInstancia().setAtendido(TipoBooleanEnum.F);
 		return super.enviar();
+	}
+	
+	@Override
+	public void aposEnviar() {
+//		enviarEmail();  
+		super.aposEnviar();
+	}
+
+	private void enviarEmail() {
+		try {
+			HtmlEmail email = new HtmlEmail();  
+	        
+	        // adiciona uma imagem ao corpo da mensagem e retorna seu id  
+	        
+	        // configura a mensagem para o formato HTML  
+	        email.setHtmlMsg("<html>Logo do Apache - <img ></html>");  
+	  
+	        // configure uma mensagem alternativa caso o servidor não suporte HTML  
+	        email.setTextMsg("Seu servidor de e-mail não suporta mensagem HTML");  
+	          
+	        email.setHostName("smtp.gmail.com"); // o servidor SMTP para envio do e-mail  
+	        email.addTo("marlonmca@gmail.com", "Márlon Assis"); //destinatário  
+	        email.setFrom("farmaciahusergipe@gmail.com", "Eu"); // remetente  
+	        email.setSubject("Teste -> Html Email"); // assunto do e-mail  
+	        email.setMsg("Teste de Email HTML utilizando commons-email"); //conteudo do e-mail  
+	        email.setAuthentication("farmaciahusergipe@gmail.com", "farmaciahusergipe2012");  
+	        email.setSmtpPort(465);  
+	        email.setSSL(true);  
+	        email.setTLS(true);  
+	        // envia email  
+	        email.send();
+		} catch (EmailException e) {
+			e.printStackTrace();
+		}  
 	}
 }
