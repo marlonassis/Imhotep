905d7ee69c80066523ab2c1052ecdd030c1169f0
marlonassis <marlonassis@gmail.com>
2012-04-11 14:03:12 -0300

    Criado a forma de bloqueio automático do lote.

--- ControleDispensacao/JavaSource/br/com/ControleDispensacao/negocio/BloqueioLoteHome.java
+++ ControleDispensacao/JavaSource/br/com/ControleDispensacao/negocio/BloqueioLoteHome.java
@@ -1,6 +1,9 @@
 package br.com.ControleDispensacao.negocio;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -16,6 +19,12 @@ import br.com.nucleo.PadraoHome;
 @SessionScoped
 public class BloqueioLoteHome extends PadraoHome<Estoque>{
 	
+	public List<Estoque> listaEstoqueVencido(){
+		Calendar dataFutura = Calendar.getInstance();
+		dataFutura.add(Calendar.DAY_OF_MONTH, 5);
+		return getBusca("select o from Estoque o where o.bloqueado = 'N' and o.dataValidade <= '"+new SimpleDateFormat("yyyy-MM-hh").format(dataFutura.getTime())+"' order by o.dataValidade"); 
+	}
+	
 	@Override
 	public boolean enviar() {
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Não é permitido inserir um estoque", "Inserção não autorizada."));
@@ -26,6 +35,12 @@ public class BloqueioLoteHome extends PadraoHome<Estoque>{
 	public boolean apagar() {
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Não é permitido apagar um estoque", "Deleção não autorizada."));
 		return false;
+	}
+	
+	public void bloqueioAutomáticoEstoque(){
+		getInstancia().setBloqueado(TipoStatusEnum.S);
+		getInstancia().setMotivoBloqueio("Bloqueio automático. Lote vencido.");
+		atualizar();
 	}
 	
 	@Override
