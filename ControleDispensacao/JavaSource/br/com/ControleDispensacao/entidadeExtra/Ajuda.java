f050bbd8720fa1b4bafa8a7eea3f9a2d8fdb235e
marlonassis <marlonassis@gmail.com>
2012-06-02 11:35:22 -0300

    Criada a entidade Ajuda.

---
+++ ControleDispensacao/JavaSource/br/com/ControleDispensacao/entidadeExtra/Ajuda.java
@@ -0,0 +1,31 @@
+package br.com.ControleDispensacao.entidadeExtra;
+
+public class Ajuda {
+	private int idAjuda;
+	private String nomeMenu;
+	private String template;
+	
+	public int getIdAjuda() {
+		return idAjuda;
+	}
+	
+	public void setIdAjuda(int idAjuda) {
+		this.idAjuda = idAjuda;
+	}
+	
+	public String getNomeMenu() {
+		return nomeMenu;
+	}
+	
+	public void setNomeMenu(String nomeMenu) {
+		this.nomeMenu = nomeMenu;
+	}
+	
+	public String getTemplate() {
+		return template;
+	}
+	
+	public void setTemplate(String template) {
+		this.template = template;
+	}
+}
---
+++ ControleDispensacao/JavaSource/br/com/ControleDispensacao/negocio/AjudaHome.java
@@ -0,0 +1,37 @@
+package br.com.ControleDispensacao.negocio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.ControleDispensacao.entidadeExtra.Ajuda;
+
+@ManagedBean(name="ajudaHome")
+@SessionScoped
+public class AjudaHome{
+	
+	private List<Ajuda> ajudaList = new ArrayList<Ajuda>();
+	private String nomeMenuConsulta;
+	private final String URL_TEMPLATE = "/ControleDispensacao/PaginasWeb/Ajuda/Template/";
+	
+	public List<Ajuda> getListaAjuda(){
+		ajudaList = new ArrayList<Ajuda>();
+		Ajuda ajuda = new Ajuda();
+		ajuda.setIdAjuda(1);
+		ajuda.setNomeMenu("Cadastro de Material");
+		ajuda.setTemplate(URL_TEMPLATE + "cadastroMaterial.jsf");
+		ajudaList.add(ajuda);
+		return ajudaList;
+	}
+	
+	public String getNomeMenuConsulta() {
+		return nomeMenuConsulta;
+	}
+
+	public void setNomeMenuConsulta(String nomeMenuConsulta) {
+		this.nomeMenuConsulta = nomeMenuConsulta;
+	}
+	
+}
