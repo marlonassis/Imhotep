aecca38c46cd6c0000bafc62c90fb178619f8b1d
marlonassis <marlonassis@gmail.com>
2012-06-03 16:42:32 -0300

    Criado a entidade para controlar o menu que usuário tem acesso.

---
+++ ControleDispensacao/JavaSource/br/com/ControleDispensacao/auxiliar/ControleMenu.java
@@ -0,0 +1,54 @@
+package br.com.ControleDispensacao.auxiliar;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.ControleDispensacao.entidade.Menu;
+import br.com.nucleo.utilidades.Utilities;
+
+@ManagedBean(name="controleMenu")
+@SessionScoped
+public class ControleMenu implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private List<String> menuAutorizadoStringList = new ArrayList<String>();
+	private List<Menu> menuAutorizadoList = new ArrayList<Menu>();
+	
+	public static ControleMenu getInstancia(){
+		try {
+			return (ControleMenu) Utilities.procuraInstancia(ControleMenu.class);
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void converteMenuString(){
+		setMenuAutorizadoStringList(new ArrayList<String>());
+		for(Menu menu : menuAutorizadoList){
+			getMenuAutorizadoStringList().add(menu.getDescricao());
+		}
+	}
+	
+	public List<Menu> getMenuAutorizadoList() {
+		return menuAutorizadoList;
+	}
+	public void setMenuAutorizadoList(List<Menu> menuAutorizadoList) {
+		this.menuAutorizadoList = menuAutorizadoList;
+	}
+	public List<String> getMenuAutorizadoStringList() {
+		return menuAutorizadoStringList;
+	}
+	public void setMenuAutorizadoStringList(List<String> menuAutorizadoStringList) {
+		this.menuAutorizadoStringList = menuAutorizadoStringList;
+	}
+	
+}
