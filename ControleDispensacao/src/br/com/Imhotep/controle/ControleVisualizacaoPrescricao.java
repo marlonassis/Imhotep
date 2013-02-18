3fff95b301b34d8ef86b0dfd5e5482f2e619cd1a
marlonassis <marlonassis@gmail.com>
2013-02-18 17:26:13 -0300

    Remoção de import desnecessário.

--- ControleDispensacao/src/br/com/Imhotep/controle/ControleVisualizacaoPrescricao.java
+++ ControleDispensacao/src/br/com/Imhotep/controle/ControleVisualizacaoPrescricao.java
@@ -1,6 +1,5 @@
 package br.com.Imhotep.controle;
 
-import br.com.Imhotep.entidade.Prescricao;
 import br.com.remendo.PadraoControle;
 
 public class ControleVisualizacaoPrescricao extends PadraoControle {
