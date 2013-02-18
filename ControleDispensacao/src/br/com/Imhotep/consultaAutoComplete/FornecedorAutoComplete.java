98b62824723ce598e62de299548400830da67b12
marlonassis <marlonassis@gmail.com>
2013-02-18 12:27:09 -0300

    classes compiladas.

---
+++ ControleDispensacao/src/br/com/Imhotep/consultaAutoComplete/FornecedorAutoComplete.java
@@ -0,0 +1,20 @@
+package br.com.Imhotep.consultaAutoComplete;
+
+import java.util.Collection;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import br.com.Imhotep.entidade.Fornecedor;
+import br.com.remendo.ConsultaGeral;
+
+@ManagedBean(name="fornecedorAutoComplete")
+@RequestScoped
+public class FornecedorAutoComplete extends ConsultaGeral<Fornecedor> {
+	
+	public Collection<Fornecedor> autoComplete(String string){
+		StringBuilder stringB = new StringBuilder("select o from Fornecedor o where lower(to_ascii(o.nome)) like lower(to_ascii('%"+string+"%'))");
+		return super.consulta(stringB, null);
+	}
+	
+}
