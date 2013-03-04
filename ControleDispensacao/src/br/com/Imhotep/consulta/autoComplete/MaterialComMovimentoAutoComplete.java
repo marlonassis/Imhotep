603a7e33f3d8e26126cc1feee94d3ad064c2fba8
Marlon Assis <marlonassis@gmail.com>
2013-03-04 16:22:07 -0300

    Criada classe autoComplete para buscar todos os materiais que possuem movimento.

---
+++ ControleDispensacao/src/br/com/Imhotep/consulta/autoComplete/MaterialComMovimentoAutoComplete.java
@@ -0,0 +1,27 @@
+package br.com.Imhotep.consulta.autoComplete;
+
+import java.util.Collection;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import br.com.Imhotep.auxiliar.Utilities;
+import br.com.Imhotep.entidade.Material;
+import br.com.remendo.ConsultaGeral;
+
+@ManagedBean(name="materialComMovimentoAutoComplete")
+@RequestScoped
+public class MaterialComMovimentoAutoComplete extends ConsultaGeral<Material> {
+	
+	public Collection<Material> autoComplete(String string){
+		StringBuilder stringB = new StringBuilder("select distinct o.material from MovimentoLivro o where ");
+		
+		if(Utilities.isNumero(string)){
+			stringB.append("o.material.codigoMaterial = "+string);
+		}else{
+			stringB.append("lower(to_ascii(o.material.descricao)) like lower(to_ascii('%"+string+"%'))");
+		}
+		return super.consulta(stringB, null);
+	}
+	
+}
