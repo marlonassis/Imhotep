401aee5edfc5d57a69abf3d0d1b1cbfdb157f821
Marlon Assis <marlonassis@gmail.com>
2013-03-04 12:53:41 -0300

    Criada a classe para consulta das doações.

---
+++ ControleDispensacao/src/br/com/Imhotep/consulta/entidade/DoacaoConsulta.java
@@ -0,0 +1,26 @@
+package br.com.Imhotep.consulta.entidade;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.Imhotep.entidade.Doacao;
+import br.com.remendo.ConsultaGeral;
+import br.com.remendo.PadraoConsulta;
+
+@ManagedBean(name="doacaoConsulta")
+@SessionScoped
+public class DoacaoConsulta extends PadraoConsulta<Doacao> {
+	public DoacaoConsulta(){
+		getCamposConsulta().put("o.hospital", IGUAL);
+		setOrderBy("o.dataDoacao");
+	}
+	
+	@Override
+	public List<Doacao> getList() {
+		setConsultaGeral(new ConsultaGeral<Doacao>());
+		getConsultaGeral().setSqlConsultaSB(new StringBuilder("select o from Doacao o where 1=1"));
+		return super.getList();
+	}
+}
