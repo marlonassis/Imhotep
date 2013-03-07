66d8ac117a651156d66b0a3af5b4f7d4d4510d0c
marlonassis-farmacia <marlonassis@gmail.com>
2013-03-07 10:13:12 -0300

    Criada a classe responsável por fazer o select do tipo de movimento para dispensação simples.

---
+++ ControleDispensacao/src/br/com/imhotep/consulta/selectOne/TipoMovimentoDispensacaoSimplesSelectOne.java
@@ -0,0 +1,21 @@
+package br.com.imhotep.consulta.selectOne;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import br.com.Imhotep.entidade.TipoMovimento;
+import br.com.remendo.ConsultaGeral;
+
+@ManagedBean(name="tipoMovimentoDispensacaoSimplesSelectOne")
+@RequestScoped
+public class TipoMovimentoDispensacaoSimplesSelectOne extends ConsultaGeral<TipoMovimento> {
+	
+	public List<TipoMovimento> getItens(){
+		StringBuilder stringB = new StringBuilder("select o from TipoMovimento o where o.idTipoMovimento = 21 or o.idTipoMovimento = 22");
+		return new ArrayList<TipoMovimento>(super.consulta(stringB, null));
+	}
+	
+}
