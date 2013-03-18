9f36f58f5c473de71f30d7ae8116ce441fb5f56b
Marlon Assis <marlonassis@gmail.com>
2013-03-18 09:33:19 -0300

    Reestruturado o a controladora de estoque para atender às demandas de prescrição e ajuste de estoque.

--- ControleDispensacao/src/br/com/Imhotep/controle/ControleEstoque.java
+++ ControleDispensacao/src/br/com/Imhotep/controle/ControleEstoque.java
@@ -1,52 +1,110 @@
 package br.com.Imhotep.controle;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-
-import javax.faces.application.FacesMessage;
+import java.util.HashMap;
 
 import br.com.Imhotep.entidade.Estoque;
+import br.com.Imhotep.entidade.Material;
 import br.com.Imhotep.entidade.MovimentoLivro;
 import br.com.Imhotep.entidade.TipoMovimento;
 import br.com.Imhotep.enums.TipoOperacaoEnum;
 import br.com.Imhotep.seguranca.Autenticador;
 import br.com.imhotep.consulta.raiz.MovimentoLivroConsultaRaiz;
+import br.com.imhotep.excecoes.ExcecaoEstoqueBloqueado;
+import br.com.imhotep.excecoes.ExcecaoEstoqueReservado;
+import br.com.imhotep.excecoes.ExcecaoEstoqueVazio;
+import br.com.imhotep.excecoes.ExcecaoEstoqueVencido;
+import br.com.imhotep.excecoes.ExcecaoSaldoInsuficienteEstoque;
 import br.com.imhotep.temp.PadraoGeralTemp;
+import br.com.remendo.ConsultaGeral;
 
 public class ControleEstoque extends PadraoGeralTemp {
 	
-	public boolean estoqueInsuficiente(Integer quantidadeAtual, int quantidadeMovimentacao) {
-		if(quantidadeMovimentacao > quantidadeAtual){
-			super.mensagem("Não há quantidade suficiente no estoque. O máximo disponível é de " + String.valueOf(quantidadeAtual) + " unidade(s)", "", FacesMessage.SEVERITY_ERROR);
-			return true;
-		}else{
-			return false;
-		}
+	public void estoqueReservado(int sobra, int reservado) throws ExcecaoEstoqueReservado {
+		if(reservado > sobra)
+			throw new ExcecaoEstoqueReservado(reservado);
+	}
+	
+	public void estoqueInsuficiente(int totalEstoque, int quantidadeMovimentacao) throws ExcecaoSaldoInsuficienteEstoque {
+		if(quantidadeMovimentacao > totalEstoque)
+			throw new ExcecaoSaldoInsuficienteEstoque(totalEstoque);
 	}
 
-	public boolean estoqueVazio(Integer quantidade) {
-		if(quantidade > 0){
-			return false;
-		}else{
-			super.mensagem("Este medicamento está em falta.", "", FacesMessage.SEVERITY_ERROR);
-			return true;
+	public void estoqueVazio(Integer quantidade) throws ExcecaoEstoqueVazio {
+		if(quantidade == 0)
+			throw new ExcecaoEstoqueVazio();
+	}
+	
+	private void estoqueBloqueado(Estoque estoque) throws ExcecaoEstoqueBloqueado{
+		if(estoque.getBloqueado())
+			throw new ExcecaoEstoqueBloqueado();
+	}
+	
+	private void estoqueVencido(Estoque estoque) throws ExcecaoEstoqueVencido {
+		Calendar dataAtual = zerarHoraDataAtual();
+		Calendar dataVencimento = setarUltimoDiaMesVencimento(estoque, dataAtual);
+		if(dataAtual.after(dataVencimento))
+			throw new ExcecaoEstoqueVencido();
+	}
+
+	private Calendar setarUltimoDiaMesVencimento(Estoque estoque, Calendar dataAtual) {
+		Calendar dataVencimento = dataAtual;
+		dataVencimento.setTime(estoque.getDataValidade());
+		dataVencimento.set(Calendar.DAY_OF_MONTH, dataVencimento.getActualMaximum(Calendar.DAY_OF_MONTH));
+		return dataVencimento;
+	}
+
+	private Calendar zerarHoraDataAtual() {
+		Calendar dataAtual = Calendar.getInstance();
+		dataAtual.set(Calendar.HOUR_OF_DAY, 0);
+		dataAtual.set(Calendar.MINUTE, 0);
+		dataAtual.set(Calendar.SECOND, 0);
+		return dataAtual;
+	}
+	
+	private Object[] consultaEstoqueMaterial(Material material) {
+		String mesAnoAtual = new SimpleDateFormat("yyyy-MM").format(Calendar.getInstance().getTime());
+		StringBuilder sb = new StringBuilder("select CASE WHEN sum(o.quantidade) = null THEN 0 ELSE sum(o.quantidade)END, ");
+		sb.append("(select CASE WHEN sum(a.quantidade) = null THEN 0 ELSE sum(a.quantidade) END ");
+		sb.append("from PrescricaoItemDose a where a.prescricaoItem.dispensado = 'N' and a.prescricaoItem.status = 'S' and a.prescricaoItem.material.idMaterial = :idMaterial) ");
+		sb.append("from Estoque o where o.material.idMaterial = :idMaterial and o.bloqueado = false and to_char(o.dataValidade, 'yyyy-MM') > :dataAtual");
+		HashMap<Object, Object> map = new HashMap<Object, Object>();
+		map.put("idMaterial", material.getIdMaterial());
+		map.put("dataAtual", mesAnoAtual);
+		Object[] qtds = new ConsultaGeral<Object[]>().consultaUnica(sb, map);
+		return qtds;
+	}
+	
+	private void filtroEstoque(Estoque estoque, TipoOperacaoEnum tipoOperacao, int quantidadeSolicitada) throws ExcecaoEstoqueVencido, ExcecaoEstoqueBloqueado, ExcecaoEstoqueVazio, ExcecaoSaldoInsuficienteEstoque, ExcecaoEstoqueReservado{
+		estoqueVencido(estoque);
+		estoqueBloqueado(estoque);
+		if(!tipoOperacao.equals(TipoOperacaoEnum.E)){
+			estoqueVazio(estoque.getQuantidadeAtual());
+			Object[] qtds = consultaEstoqueMaterial(estoque.getMaterial());
+			int totalEstoque = (Integer) qtds[0];
+			int totalReservardo = (Integer) qtds[1];
+			estoqueInsuficiente(estoque.getQuantidadeAtual(), quantidadeSolicitada);
+			int sobra = totalEstoque-quantidadeSolicitada;
+			estoqueReservado(sobra, totalReservardo);
 		}
 	}
 	
-	private boolean estoqueBloqueado(Estoque estoque){
-		if(estoque.getBloqueado()){
-			super.mensagem("Este estoque está bloqueado.", "", FacesMessage.SEVERITY_ERROR);
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean manipularEstoque(Date dataAtual, MovimentoLivro movimentoLivro) throws InstantiationException, IllegalAccessException,ClassNotFoundException {
+	public void liberarAjuste(Date dataAtual, MovimentoLivro movimentoLivro) throws ExcecaoEstoqueVencido, ExcecaoEstoqueBloqueado, ExcecaoEstoqueVazio, ExcecaoSaldoInsuficienteEstoque, ExcecaoEstoqueReservado, InstantiationException, IllegalAccessException, ClassNotFoundException {
+		filtroEstoque(movimentoLivro.getEstoque(), movimentoLivro.getTipoMovimento().getTipoOperacao(), movimentoLivro.getQuantidadeMovimentacao());
 		upperCaseLote(movimentoLivro.getEstoque().getLote());
-		if(!estoqueBloqueado(movimentoLivro.getEstoque())){
-			prepararMovimentoLivro(dataAtual, movimentoLivro);
-			return prepararEstoque(dataAtual, movimentoLivro.getEstoque(), movimentoLivro.getQuantidadeMovimentacao(), movimentoLivro.getTipoMovimento());
-		}
-		return false;
+		prepararMovimentoLivro(dataAtual, movimentoLivro);
+		prepararEstoque(dataAtual, movimentoLivro.getEstoque(), movimentoLivro.getQuantidadeMovimentacao(), movimentoLivro.getTipoMovimento());
+	}
+	
+	public void liberarDose(int quantidadeDose, Material material) throws ExcecaoEstoqueVazio, ExcecaoSaldoInsuficienteEstoque {
+		Object[] qtds = consultaEstoqueMaterial(material);
+		int totalEstoque = (Integer) qtds[0];
+		int totalReservardo = (Integer) qtds[1];
+		int totalVirtual = totalEstoque-totalReservardo;
+		estoqueVazio(totalEstoque);
+		estoqueInsuficiente(totalVirtual, quantidadeDose);
 	}
 	
 	private void upperCaseLote(String lote) {
@@ -62,22 +120,18 @@ public class ControleEstoque extends PadraoGeralTemp {
 		movimentoLivro.setUsuarioMovimentacao(autenticador.getUsuarioAtual());
 	}
 
-	private boolean prepararEstoque(Date dataAtual, Estoque estoque, int quantidadeMovimentada, TipoMovimento tipoMovimento) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+	private void prepararEstoque(Date dataAtual, Estoque estoque, int quantidadeMovimentada, TipoMovimento tipoMovimento) throws InstantiationException, IllegalAccessException, ClassNotFoundException, ExcecaoSaldoInsuficienteEstoque, ExcecaoEstoqueVazio {
 		if(estoque.getIdEstoque() == 0)
 			setarDadosNovoEstoque(estoque, dataAtual);
 		int saldoAtualizado = 0;
 		int saldoAtual = estoque.getQuantidadeAtual();
 		boolean movimentoEntrada = tipoMovimento.getTipoOperacao().equals(TipoOperacaoEnum.E);
-		if(movimentoEntrada || (!estoqueVazio(saldoAtual) && !estoqueInsuficiente(saldoAtual, quantidadeMovimentada))){
-			if(movimentoEntrada){
-				saldoAtualizado = saldoAtual + quantidadeMovimentada;
-			}else{
-				saldoAtualizado = saldoAtual - quantidadeMovimentada;
-			}
-			estoque.setQuantidadeAtual(saldoAtualizado);
-			return true;
+		if(movimentoEntrada){
+			saldoAtualizado = saldoAtual + quantidadeMovimentada;
+		}else{
+			saldoAtualizado = saldoAtual - quantidadeMovimentada;
 		}
-		return false;
+		estoque.setQuantidadeAtual(saldoAtualizado);
 	}
 
 	private void setarDadosNovoEstoque(Estoque estoque, Date dataAtual) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
