1a46e1cca1f01344e8d68c585544b02513641b73
Marlon Assis <marlonassis@gmail.com>
2013-03-28 16:33:53 -0300

    Criado método para gerar relatórios repassando apenas um objeto.

--- ControleDispensacao/src/br/com/Imhotep/relatorio/PadraoRelatorio.java
+++ ControleDispensacao/src/br/com/Imhotep/relatorio/PadraoRelatorio.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,21 +14,16 @@ import javax.faces.context.FacesContext;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
-import br.com.Imhotep.auxiliar.Constantes;
-
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import br.com.Imhotep.auxiliar.Constantes;
 
 public class PadraoRelatorio implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-    protected void geraRelatorio(String caminho, String nomeRelatorio) throws ClassNotFoundException, IOException, JRException, SQLException {
-		geraRelatorio(caminho, nomeRelatorio, null, null);
-	}
-	
 	@SuppressWarnings("rawtypes")
     protected void geraRelatorio(String caminho, String nomeRelatorio, List list, Map<String, Object> map) throws ClassNotFoundException, IOException, JRException, SQLException {
     	String url = Constantes.URL_BANCO;
@@ -55,6 +51,36 @@ public class PadraoRelatorio implements Serializable {
         	//caso a lista esteja nula os itens vem do próprio arquivo
         	JasperRunManager.runReportToPdfStream(reportStream, servletOutputStream, map, con);
         }
+        
+        con.close();
+        servletOutputStream.flush();
+        servletOutputStream.close();
+
+    }
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	protected void geraRelatorio(String caminho, String nomeRelatorio, Object obj, Map<String, Object> map) throws ClassNotFoundException, IOException, JRException, SQLException {
+    	String url = Constantes.URL_BANCO;
+		String usuario = Constantes.USUARIO_BANCO;
+		String senha = Constantes.SENHA_BANCO;
+		Class.forName("org.postgresql.Driver"); 
+		Connection con = DriverManager.getConnection(url, usuario, senha);
+		
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        
+        HttpServletResponse response = (HttpServletResponse) ctx.getExternalContext().getResponse();
+        
+		InputStream reportStream = ctx.getExternalContext().getResourceAsStream(caminho);
+		
+        ServletOutputStream servletOutputStream = response.getOutputStream();
+        
+        ctx.responseComplete();
+        response.setContentType("application/pdf");
+        response.addHeader("Content-disposition", "filename="+nomeRelatorio);
+        
+		List list = new ArrayList();
+        list.add(obj);
+		JasperRunManager.runReportToPdfStream(reportStream, servletOutputStream, map, (JRDataSource) new JRBeanCollectionDataSource(list));
         
         con.close();
         servletOutputStream.flush();
