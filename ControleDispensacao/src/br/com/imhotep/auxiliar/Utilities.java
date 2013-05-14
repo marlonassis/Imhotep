7d19cbcd7dfacb9df2823c482326de5e49df6086
marlonassis <marlonassis@gmail.com>
2013-05-14 00:20:36 -0300

    Adicionado novos métodos-ferramentas para facilitar acoes básicas.

--- ControleDispensacao/src/br/com/imhotep/auxiliar/Utilities.java
+++ ControleDispensacao/src/br/com/imhotep/auxiliar/Utilities.java
@@ -1,7 +1,10 @@
 package br.com.imhotep.auxiliar;
 
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.text.NumberFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -12,6 +15,7 @@ import java.util.regex.Pattern;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.persistence.Id;
 
 
 /**
@@ -46,6 +50,39 @@ public class Utilities extends br.com.remendo.utilidades.Utilities{
 			NumberFormat nf = NumberFormat.getInstance(LOCALE_BRASIL);
 			nf.setMinimumFractionDigits(2);
 			return nf.format(valor);
+		}
+		return null;
+	}
+	
+	public static String utf8_to_latin1(String str){
+		if(str != null){
+	       try{
+	          String stringToConvert = str;
+	          byte[] convertStringToByte = stringToConvert.getBytes("UTF-8");
+	          return new String(convertStringToByte, "ISO-8859-1");
+	       }catch(Exception e){
+	    	   System.out.println(e.getStackTrace());
+	       }
+		}
+		return null;
+	}
+	
+	public static Double StringParaDoubleDiv100(String valor){
+		if(valor != null){
+			return StringParaDouble(valor) /100.00d;
+		}
+		return null;
+	}
+	
+	public static Double StringParaDouble(String valor){
+		if(valor != null){
+			NumberFormat nf = NumberFormat.getInstance(LOCALE_BRASIL);
+			nf.setMinimumFractionDigits(2);
+			try {
+				return (nf.parse(valor).doubleValue());
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
 		}
 		return null;
 	}
@@ -98,5 +135,60 @@ public class Utilities extends br.com.remendo.utilidades.Utilities{
 	    Matcher mat = pat.matcher(texto);  
 	    return mat.matches();  
 	} 
+	
+	public static String getNomePropriedadeId(Object obj){
+		String nomeMetodo = getNomeMetodoId(obj);
+		String nomePropriedade = nomeMetodo.replace("get", "");
+		char[] nome = nomePropriedade.toCharArray();
+		nome[0] = String.valueOf(nome[0]).toLowerCase().toCharArray()[0];
+		return String.copyValueOf(nome);
+	}
+	
+	public static String getNomeMetodoId(Object obj){
+		Class<?> clazz;
+		try {
+			clazz = Class.forName(obj.getClass().getName());
+			for(Method method : clazz.getMethods()){
+				if(method.isAnnotationPresent(Id.class)){
+					return method.getName();
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static Object getValorPropriedadeId(Object obj){
+		Class<?> clazz;
+		try {
+			clazz = Class.forName(obj.getClass().getName());
+			String metodo = getNomeMetodoId(obj);
+			if(metodo != null){
+	            Method meth = clazz.getMethod(metodo, null);  
+	        	Object valor = meth.invoke(obj, null);
+				return valor;
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static String nomeSimplesClasse(Class<?> class1) {
+		String[] split = class1.getName().split("\\.");
+		String classe = split[split.length-1];
+		return classe;
+	}
 	
 }
