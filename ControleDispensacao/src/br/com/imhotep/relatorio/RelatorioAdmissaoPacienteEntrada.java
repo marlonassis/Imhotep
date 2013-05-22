17bdf4088289e7702b99e6ff7725da197949e262
marlonassis <marlonassis@gmail.com>
2013-05-22 10:23:37 -0300

    Criado o relatório que exibe a admissão do paciente.

---
+++ ControleDispensacao/src/br/com/imhotep/relatorio/RelatorioAdmissaoPacienteEntrada.java
@@ -0,0 +1,50 @@
+package br.com.imhotep.relatorio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import net.sf.jasperreports.engine.JRException;
+import br.com.imhotep.auxiliar.Constantes;
+import br.com.imhotep.auxiliar.Parametro;
+import br.com.imhotep.entidade.PacienteEntrada;
+import br.com.remendo.ConsultaGeral;
+
+@ManagedBean
+@ViewScoped
+public class RelatorioAdmissaoPacienteEntrada extends PadraoRelatorio{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public void gerarRelatorio(String id) throws ClassNotFoundException, IOException, JRException, SQLException {
+		gerar(new ConsultaGeral<PacienteEntrada>(new StringBuilder("select o from PacienteEntrada o where o.idPacienteEntrada = "+id)).consultaUnica());
+	}
+	
+	public void gerarRelatorio(PacienteEntrada pacienteEntrada) throws ClassNotFoundException, IOException, JRException, SQLException {
+		gerar(pacienteEntrada);
+	}
+	
+	private void gerar(PacienteEntrada pacienteEntrada) throws ClassNotFoundException, IOException, JRException, SQLException {
+		
+		String nomeRelatorio = pacienteEntrada.getPaciente().getNome().concat("-").concat(new SimpleDateFormat("dd-MM-yyyy").format(pacienteEntrada.getDataAtendimento()));
+		String caminho = Constantes.DIR_RELATORIO + "AdmissaoPacienteEntrada.jasper";
+		
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		//injetando o relatório com o(s) cid(s)
+		InputStream subInputStreamCid = this.getClass().getResourceAsStream("AdmissaoPacienteEntradaCid.jasper");
+		map.put("SUBREPORT_INPUT_STREAM_CID", subInputStreamCid);
+		//injetando o relatório de doses do(s) procedimento(s)
+		InputStream subInputStreamProcedimento = this.getClass().getResourceAsStream("AdmissaoPacienteEntradaProcedimento.jasper");
+		map.put("SUBREPORT_INPUT_STREAM_PROCEDIMENTO_SAUDE", subInputStreamProcedimento);
+		byte[] logoTipoHU = Parametro.logoTipoHU();
+		map.put("LOGO_HU", logoTipoHU);
+		
+		super.geraRelatorio(caminho, nomeRelatorio, pacienteEntrada, map);
+	}
+	
+}
