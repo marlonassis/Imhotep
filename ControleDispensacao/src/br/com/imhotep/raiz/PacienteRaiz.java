b3fdbf4f6477a046ee969557d313ae888763e5a6
marlonassis <marlonassis@gmail.com>
2013-05-24 18:05:21 -0300

    Melhoria nos cadastros

--- ControleDispensacao/src/br/com/imhotep/raiz/PacienteRaiz.java
+++ ControleDispensacao/src/br/com/imhotep/raiz/PacienteRaiz.java
@@ -17,9 +17,18 @@ import br.com.imhotep.seguranca.Autenticador;
 import br.com.remendo.ConsultaGeral;
 import br.com.remendo.PadraoHome;
 
-@ManagedBean(name="pacienteRaiz")
+@ManagedBean
 @SessionScoped
 public class PacienteRaiz extends PadraoHome<Paciente>{
+	
+	private String valorPesquisa;
+	private Boolean achouPaciente;
+	
+	@Override
+	public void novaInstancia() {
+		super.novaInstancia();
+		setAchouPaciente(null);
+	}
 	
 	public static PacienteRaiz getInstanciaHome() throws InstantiationException, IllegalAccessException, ClassNotFoundException{
 		return (PacienteRaiz) new ControleInstancia().procuraInstancia(PacienteRaiz.class);
@@ -27,17 +36,17 @@ public class PacienteRaiz extends PadraoHome<Paciente>{
 	
 	public void procurarPaciente() throws IOException{
 		try {
-			String numeroSus = getInstancia().getNumeroSus();
 			ConsultaGeral<Paciente> cg = new ConsultaGeral<Paciente>();
 			HashMap<Object, Object> hm = new HashMap<Object, Object>();
-			hm.put("numeroSus", numeroSus);
-			Paciente paciente = cg.consultaUnica(new StringBuilder("select o from Paciente o where o.numeroSus = :numeroSus"), hm);
+			hm.put("valor", getValorPesquisa());
+			Paciente paciente = cg.consultaUnica(new StringBuilder("select o from Paciente o where o.numeroSus = :valor or o.prontuario = :valor"), hm);
 			if(paciente != null){
-				PacienteEntradaRaiz.getInstanciaHome().setNumeroSus(numeroSus);
+				PacienteEntradaRaiz.getInstanciaHome().setValorPesquisa(getValorPesquisa());
 				PacienteEntradaRaiz.getInstanciaHome().getInstancia().setPaciente(paciente);
 				PacienteEntradaRaiz.getInstanciaHome().carregarUltimoAntendimento();
 				FacesContext.getCurrentInstance().getExternalContext().redirect(Constantes.PAGINA_ENTRADA_PACIENTE);
 			}else{
+				setAchouPaciente(false);
 				super.mensagem("O número do SUS informado não está cadastrado.", "Verifique se você informou o número certo ou cadastre esse novo usuário.", FacesMessage.SEVERITY_ERROR);
 			}
 		} catch (Exception e) {
@@ -91,6 +100,22 @@ public class PacienteRaiz extends PadraoHome<Paciente>{
 		}
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Você não está alocado em uma unidade", "Escolha uma unidade na combo acima do menu."));
 		return false;
+	}
+
+	public String getValorPesquisa() {
+		return valorPesquisa;
+	}
+
+	public void setValorPesquisa(String valorPesquisa) {
+		this.valorPesquisa = valorPesquisa;
+	}
+
+	public Boolean getAchouPaciente() {
+		return achouPaciente;
+	}
+
+	public void setAchouPaciente(Boolean achouPaciente) {
+		this.achouPaciente = achouPaciente;
 	}
 	
 }
