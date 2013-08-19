7e80f0a80bdf720ee070b799a8323418898a83fb
marlonassis <marlonassis@gmail.com>
2013-08-19 17:47:20 -0300

    Mudança de nomes das exceções.

---
+++ ControleDispensacao/src/br/com/imhotep/comparador/EstoqueDispensacaoLoteComparador.java
@@ -0,0 +1,12 @@
+package br.com.imhotep.comparador;
+
+import java.util.Comparator;
+
+import br.com.imhotep.entidade.extra.EstoqueDispensacao;
+
+public class EstoqueDispensacaoLoteComparador implements Comparator<EstoqueDispensacao>  {  
+	@Override
+	public int compare(EstoqueDispensacao arg0, EstoqueDispensacao arg1) {
+		return arg0.getEstoque().getLote().compareTo(arg1.getEstoque().getLote());
+	}
+}  
---
+++ ControleDispensacao/src/br/com/imhotep/excecoes/ExcecaoUsuarioTrocaSenha.java
@@ -0,0 +1,18 @@
+/**
+ * Criado por Márlon Assis
+ */
+package br.com.imhotep.excecoes;
+
+
+/**
+ * @author marlonassis
+ *
+ */
+public class ExcecaoUsuarioTrocaSenha extends ExcecaoPadrao {
+
+	private static final long serialVersionUID = 1783207019548721738L;
+	
+	public ExcecaoUsuarioTrocaSenha(){
+	}
+	
+}
