c4fd9dc0f1a58e6373dbad7c8ff2f3a34afb32c6
marlonassis <marlonassis@gmail.com>
2013-08-24 12:29:30 -0300

    Adicionado o pattern da chave única.

--- ControleDispensacao/src/br/com/imhotep/auxiliar/Utilitarios.java
+++ ControleDispensacao/src/br/com/imhotep/auxiliar/Utilitarios.java
@@ -1,8 +1,12 @@
 package br.com.imhotep.auxiliar;
 
 
+import java.text.SimpleDateFormat;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+
+import br.com.imhotep.entidade.Profissional;
 
 
 /**
@@ -13,5 +17,11 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class Utilitarios extends br.com.remendo.utilidades.Utilitarios {
 	
+	public static String getPatternChaveUnicaPreparada(Profissional profissional){
+		String valor = Constantes.PATTERN_CHAVE_VERIFICACAO_PROFISSIONAL;
+		valor = valor.replace("{cpf}", profissional.getCpf());
+		valor = valor.replace("{dataNascimento}", new SimpleDateFormat("dd/MM/yyyy").format(profissional.getDataNascimento()));
+		return valor;
+	}
 
 }
