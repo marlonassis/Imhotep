884eca2e9604046a6fba1d5a58b010390d8fb4df
marlonassis <marlonassis@gmail.com>
2013-08-24 14:46:25 -0300

    Adicionado a serialização e as seguintes propriedades: 	private String chaveVerificacao; 	private String cpf; 	private Set<Especialidade> especialidades;

--- ControleDispensacao/src/br/com/imhotep/entidade/Profissional.java
+++ ControleDispensacao/src/br/com/imhotep/entidade/Profissional.java
@@ -1,6 +1,10 @@
 package br.com.imhotep.entidade;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -11,6 +15,8 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -22,7 +28,9 @@ import br.com.imhotep.enums.TipoSituacaoEnum;
 
 @Entity
 @Table(name = "tb_profissional")
-public class Profissional {
+public class Profissional implements Serializable{
+	private static final long serialVersionUID = 2823381085636103344L;
+	
 	private int idProfissional;
 	private Estado estado; 
 	private Especialidade especialidade;
@@ -34,6 +42,10 @@ public class Profissional {
 	private String registroConselho;
 	private Integer matricula;
 	private Date dataNascimento;
+	private String chaveVerificacao;
+	private String cpf;
+	private Set<Especialidade> especialidades;
+	
 	
 	@SequenceGenerator(name = "generator", sequenceName = "public.tb_profissional_id_profissional_seq")
 	@Id
@@ -155,6 +167,51 @@ public class Profissional {
 		return null;
 	}
 	
+	@Column(name="cv_chave_verificacao")
+	public String getChaveVerificacao() {
+		return chaveVerificacao;
+	}
+
+	public void setChaveVerificacao(String chaveVerificacao) {
+		this.chaveVerificacao = chaveVerificacao;
+	}
+
+	@Transient
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+//	@Transient
+//	public Set<ProfissionalEspecialidade> getEspecialidades() {
+//		if(especialidades == null || especialidades.isEmpty()) {
+//			especialidades = new HashSet<ProfissionalEspecialidade>(new GerenciadorConsultaLazy<ProfissionalEspecialidade>().consultarLista(this, new ProfissionalEspecialidade(), new HashSet<ProfissionalEspecialidade>()));
+//		}
+//		return especialidades;
+//	}
+	
+	@ManyToMany(cascade = CascadeType.REFRESH, fetch=FetchType.EAGER)
+	@JoinTable(name="tb_profissional_especialidade",
+				joinColumns={@JoinColumn(name="id_profissional")}, 
+				inverseJoinColumns={@JoinColumn(name="id_especialidade")})
+	public Set<Especialidade> getEspecialidades() {
+		return especialidades;
+	}
+	public void setEspecialidades(Set<Especialidade> especialidades) {
+		this.especialidades = especialidades;
+	}
+	
+	@Transient
+	public List<Especialidade> getEspecialidadesList(){
+		if(getEspecialidades() != null){
+			return new ArrayList<Especialidade>(getEspecialidades());
+		}
+		return new ArrayList<Especialidade>();
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if(obj == null)
@@ -168,7 +225,7 @@ public class Profissional {
 	@Override
 	public int hashCode() {
 	    int hash = 1;
-	    return hash * 31 + especialidade.hashCode() + nome.hashCode() + usuario.hashCode();
+	    return hash * 31 + ((nome == null) ? 0 : nome.hashCode()) + usuario.hashCode();
 	}
 
 	@Override
