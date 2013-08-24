61574fd197146fc4574fe38197c400cf9a9c59e0
marlonassis <marlonassis@gmail.com>
2013-08-24 12:20:26 -0300

    Alterado a entidade para carregar os dados.

--- ControleDispensacao/src/br/com/imhotep/consulta/raiz/MaterialConsultaRaiz.java
+++ ControleDispensacao/src/br/com/imhotep/consulta/raiz/MaterialConsultaRaiz.java
@@ -9,6 +9,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 import br.com.imhotep.entidade.Material;
+import br.com.imhotep.entidade.extra.MaterialFaltaEstoque;
 import br.com.imhotep.raiz.MaterialRaiz;
 import br.com.remendo.ConsultaGeral;
 
@@ -18,7 +19,7 @@ public class MaterialConsultaRaiz  extends ConsultaGeral<Material>{
 
 	public void atualizaMateriaisQuantidadeAbaixoPermitido() {
 		String dataS = new SimpleDateFormat("yyyy-MM").format(Calendar.getInstance().getTime());
-		StringBuilder stringB = new StringBuilder("select o.descricao, o.quantidadeMinima, sum(a.quantidadeAtual)");
+		StringBuilder stringB = new StringBuilder("select new br.com.imhotep.entidade.extra.MaterialFaltaEstoque(o.descricao, o.quantidadeMinima, sum(a.quantidadeAtual)) ");
 		stringB.append("from Material o ");
 		stringB.append("join o.estoques a  ");
 		stringB.append("where o.quantidadeMinima is not null and o.quantidadeMinima != 0 and ");
@@ -29,7 +30,7 @@ public class MaterialConsultaRaiz  extends ConsultaGeral<Material>{
 		stringB.append("group by o.descricao, o.quantidadeMinima ");
 		stringB.append("order by to_ascii(o.descricao) ");
 		
-		List<Material> list = new ArrayList<Material>(new ConsultaGeral<Material>().consulta(stringB, null));
+		List<MaterialFaltaEstoque> list = new ArrayList<MaterialFaltaEstoque>(new ConsultaGeral<MaterialFaltaEstoque>().consulta(stringB, null));
 		MaterialRaiz.getInstanciaAtual().setMateriaisAbaixoQuantidadeMinima(list);
 	}
 }
