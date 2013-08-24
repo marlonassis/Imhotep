127d5561e241c6224c2caef630e786889fd1e37b
marlonassis <marlonassis@gmail.com>
2013-08-24 14:34:32 -0300

    Atualização para adicionar a quantidade atual e informar o tipo de operação.

--- ControleDispensacao/src/br/com/imhotep/consulta/relatorio/ConsultaRelatorioMovimentacaoEstoqueMaterial.java
+++ ControleDispensacao/src/br/com/imhotep/consulta/relatorio/ConsultaRelatorioMovimentacaoEstoqueMaterial.java
@@ -10,21 +10,22 @@ import br.com.imhotep.entidade.Material;
 import br.com.imhotep.entidade.TipoMovimento;
 import br.com.imhotep.entidade.Unidade;
 import br.com.imhotep.entidade.relatorio.MovimentacaoEstoqueMaterial;
+import br.com.imhotep.enums.TipoOperacaoEnum;
 import br.com.remendo.ConsultaGeral;
 
 public class ConsultaRelatorioMovimentacaoEstoqueMaterial extends ConsultaGeral<Object[]> {
 	
-	public List<MovimentacaoEstoqueMaterial> consultarResultados(Material material, Date dataIni, Date dataFim, Unidade unidade, TipoMovimento tipoMovimento){
-		return consultaMovimentoPeriodo(material, dataIni, dataFim, unidade, tipoMovimento);
+	public List<MovimentacaoEstoqueMaterial> consultarResultados(Material material, Date dataIni, Date dataFim, Unidade unidade, TipoMovimento tipoMovimento, TipoOperacaoEnum tipoOperacao, boolean agruparPorLote){
+		return consultaMovimentoPeriodo(material, dataIni, dataFim, unidade, tipoMovimento, tipoOperacao, agruparPorLote);
 	}
 	
-	private List<MovimentacaoEstoqueMaterial> consultaMovimentoPeriodo(Material material, Date dataIni, Date dataFim, Unidade unidade, TipoMovimento tipoMovimento) {
+	private List<MovimentacaoEstoqueMaterial> consultaMovimentoPeriodo(Material material, Date dataIni, Date dataFim, Unidade unidade, TipoMovimento tipoMovimento, TipoOperacaoEnum tipoOperacao, boolean agruparPorLote) {
 		ConsultaGeral<MovimentacaoEstoqueMaterial> cg = new ConsultaGeral<MovimentacaoEstoqueMaterial>();
 		HashMap<Object, Object> map = new HashMap<Object, Object>();
 		map.put("dataIni", dataIni);
 		map.put("dataFim", dataFim);
 		String sql = "select new br.com.imhotep.entidade.relatorio.MovimentacaoEstoqueMaterial(o.tipoMovimento, " +
-				"a.nome, o.estoque.lote, o.quantidadeMovimentacao, o.dataMovimento, " +
+				"a.nome, o.estoque.lote, o.quantidadeMovimentacao, o.quantidadeAtual, o.dataMovimento, " +
 				"o.usuarioMovimentacao.login, o.estoque.material.descricao) from MovimentoLivro o " +
 				"LEFT OUTER JOIN o.dispensacaoSimples.unidadeDispensada a "+
 				"where o.dataMovimento >= :dataIni and o.dataMovimento <= :dataFim";
@@ -44,7 +45,16 @@ public class ConsultaRelatorioMovimentacaoEstoqueMaterial extends ConsultaGeral<
 			map.put("idTipoMovimento", tipoMovimento.getIdTipoMovimento());
 		}
 		
-		sql += " order by o.dataMovimento desc";
+		if(tipoOperacao != null){
+			sql += " and o.tipoMovimento.tipoOperacao = :tipoOperacao";
+			map.put("tipoOperacao", tipoOperacao);
+		}
+		
+		if(agruparPorLote){
+			sql += " order by o.estoque.lote, o.dataMovimento desc";
+		}else{
+			sql += " order by o.dataMovimento desc";
+		}
 		
 		ArrayList<MovimentacaoEstoqueMaterial> list = new ArrayList<MovimentacaoEstoqueMaterial>(cg.consulta(new StringBuilder(sql), map));
 		
--- ControleDispensacao/src/br/com/imhotep/consulta/relatorio/ConsultaRelatorioMovimentacaoEstoqueUnidade.java
+++ ControleDispensacao/src/br/com/imhotep/consulta/relatorio/ConsultaRelatorioMovimentacaoEstoqueUnidade.java
@@ -9,20 +9,21 @@ import br.com.imhotep.auxiliar.Constantes;
 import br.com.imhotep.entidade.TipoMovimento;
 import br.com.imhotep.entidade.Unidade;
 import br.com.imhotep.entidade.relatorio.MovimentacaoEstoqueUnidade;
+import br.com.imhotep.enums.TipoOperacaoEnum;
 import br.com.remendo.ConsultaGeral;
 
 public class ConsultaRelatorioMovimentacaoEstoqueUnidade extends ConsultaGeral<Object[]> {
 	
-	public List<MovimentacaoEstoqueUnidade> consultarResultados(Date dataIni, Date dataFim, Unidade unidade, TipoMovimento tipoMovimento){
-		return consultaMovimentoPeriodo(dataIni, dataFim, unidade, tipoMovimento);
+	public List<MovimentacaoEstoqueUnidade> consultarResultados(Date dataIni, Date dataFim, Unidade unidade, TipoMovimento tipoMovimento, TipoOperacaoEnum tipoOperacao){
+		return consultaMovimentoPeriodo(dataIni, dataFim, unidade, tipoMovimento, tipoOperacao);
 	}
 
-	private List<MovimentacaoEstoqueUnidade> consultaMovimentoPeriodo(Date dataIni, Date dataFim, Unidade unidade, TipoMovimento tipoMovimento) {
+	private List<MovimentacaoEstoqueUnidade> consultaMovimentoPeriodo(Date dataIni, Date dataFim, Unidade unidade, TipoMovimento tipoMovimento, TipoOperacaoEnum tipoOperacao) {
 		ConsultaGeral<MovimentacaoEstoqueUnidade> cg = new ConsultaGeral<MovimentacaoEstoqueUnidade>();
 		HashMap<Object, Object> map = new HashMap<Object, Object>();
 		map.put("dataIni", dataIni);
 		map.put("dataFim", dataFim);
-		String sql = "select new br.com.imhotep.entidade.relatorio.MovimentacaoEstoqueUnidade(o.movimentoLivro.tipoMovimento, o.movimentoLivro.estoque.material.descricao, o.movimentoLivro.estoque.lote, o.movimentoLivro.quantidadeMovimentacao, o.movimentoLivro.dataMovimento, o.movimentoLivro.usuarioMovimentacao.login) from DispensacaoSimples o where o.movimentoLivro.dataMovimento >= :dataIni and o.movimentoLivro.dataMovimento <= :dataFim";
+		String sql = "select new br.com.imhotep.entidade.relatorio.MovimentacaoEstoqueUnidade(o.movimentoLivro.tipoMovimento, o.movimentoLivro.estoque.material.descricao, o.movimentoLivro.estoque.lote, o.movimentoLivro.quantidadeMovimentacao, o.movimentoLivro.quantidadeAtual, o.movimentoLivro.dataMovimento, o.movimentoLivro.usuarioMovimentacao.login) from DispensacaoSimples o where o.movimentoLivro.dataMovimento >= :dataIni and o.movimentoLivro.dataMovimento <= :dataFim";
 		
 		if(unidade != null){
 			sql += " and o.unidadeDispensada.idUnidade = :idUnidade ";
@@ -32,6 +33,11 @@ public class ConsultaRelatorioMovimentacaoEstoqueUnidade extends ConsultaGeral<O
 		if(tipoMovimento != null && tipoMovimento.getIdTipoMovimento() != 0){
 			sql += " and o.movimentoLivro.tipoMovimento.idTipoMovimento = :idTipoMovimento";
 			map.put("idTipoMovimento", tipoMovimento.getIdTipoMovimento());
+		}
+		
+		if(tipoOperacao != null){
+			sql += " and o.movimentoLivro.tipoMovimento.tipoOperacao = :tipoOperacao";
+			map.put("tipoOperacao", tipoOperacao);
 		}
 		
 		sql += " order by o.movimentoLivro.dataMovimento desc";
