a0449b6cf7b8eeacb945f48b1c7023969f756dfc
marlonassis <marlonassis@gmail.com>
2013-08-24 12:27:23 -0300

    Criado o pré-cadastro de profissional para auxiliar no cadastros dos enfermeiros.

---
+++ ControleDispensacao/src/br/com/imhotep/raiz/PreCadastroProfissionalRaiz.java
@@ -0,0 +1,37 @@
+package br.com.imhotep.raiz;
+
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.imhotep.auxiliar.RestringirAcessoRedeHU;
+import br.com.imhotep.entidade.PreCadastroProfissional;
+import br.com.imhotep.excecoes.ExcecaoForaRedeHU;
+import br.com.remendo.PadraoHome;
+
+@ManagedBean
+@SessionScoped
+public class PreCadastroProfissionalRaiz extends PadraoHome<PreCadastroProfissional>{
+	
+	@Override
+	protected void preEnvio() {
+		getInstancia().setDataInsercao(new Date());
+		super.preEnvio();
+	}
+	
+	@Override
+	public boolean enviar() {
+		try {
+			new RestringirAcessoRedeHU().validarAcessoRedeHU();
+			if(super.enviar()){
+				super.novaInstancia();
+				return true;
+			}
+		} catch (ExcecaoForaRedeHU e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+}
