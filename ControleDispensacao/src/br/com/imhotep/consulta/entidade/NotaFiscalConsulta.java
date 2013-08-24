90b8fc038782399187a1abce14db89f6fa69d21c
marlonassis <marlonassis@gmail.com>
2013-08-24 14:36:20 -0300

    Código testado para verificar melhoria no desempenho da consulta.

--- ControleDispensacao/src/br/com/imhotep/consulta/entidade/NotaFiscalConsulta.java
+++ ControleDispensacao/src/br/com/imhotep/consulta/entidade/NotaFiscalConsulta.java
@@ -1,28 +1,49 @@
 package br.com.imhotep.consulta.entidade;
 
-import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import br.com.imhotep.entidade.Material;
 import br.com.imhotep.entidade.NotaFiscal;
 import br.com.remendo.ConsultaGeral;
-import br.com.remendo.PadraoConsulta;
+import br.com.imhotep.temp.PadraoConsulta;
 
-@ManagedBean(name="notaFiscalConsulta")
+@ManagedBean
 @SessionScoped
 public class NotaFiscalConsulta extends PadraoConsulta<NotaFiscal> {
+	private Material material;
+	
 	public NotaFiscalConsulta(){
 		getCamposConsulta().put("o.fornecedor", IGUAL);
-		getCamposConsulta().put("o.identificacaoNotaFiscal", CONTENDO);
+		getCamposConsulta().put("o.identificacaoNotaFiscal", INCLUINDO_TUDO);
 		setOrderBy("o.dataInsercao desc");
 	}
 	
 	@Override
-	public List<NotaFiscal> getList() {
+	public void novaInstancia() {
+		setMaterial(new Material());
+		super.novaInstancia();
+	}
+	
+	@Override
+	public void carregarResultado(){
+		setPesquisaGuiada(true);
 		setConsultaGeral(new ConsultaGeral<NotaFiscal>());
-		String sql = "select o from NotaFiscal o where 1=1";
+		String sql = null;
+		if(getMaterial() == null || getMaterial().getIdMaterial() == 0)
+			sql = "select o from NotaFiscal o where 1=1";
+		else
+			sql = "select o from NotaFiscal o join o.itens a where a.estoque.material.idMaterial = "+getMaterial().getIdMaterial();
+		
 		getConsultaGeral().setSqlConsultaSB(new StringBuilder(sql));
-		return super.getList();
+		super.carregarResultado();
+	}
+
+	public Material getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(Material material) {
+		this.material = material;
 	}
 }
