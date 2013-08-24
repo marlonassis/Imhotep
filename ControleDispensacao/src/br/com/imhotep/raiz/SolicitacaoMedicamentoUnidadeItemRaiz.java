89e2115aade891c13a52df802bbf284bbfdbe29a
marlonassis <marlonassis@gmail.com>
2013-08-24 10:47:09 -0300

    Melhorias parciais no código da solicitação.

--- ControleDispensacao/src/br/com/imhotep/raiz/SolicitacaoMedicamentoUnidadeItemRaiz.java
+++ ControleDispensacao/src/br/com/imhotep/raiz/SolicitacaoMedicamentoUnidadeItemRaiz.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -11,13 +12,12 @@ import java.util.Set;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-import org.primefaces.event.CellEditEvent;
-
 import br.com.imhotep.auxiliar.Constantes;
 import br.com.imhotep.auxiliar.Parametro;
 import br.com.imhotep.auxiliar.Utilitarios;
-import br.com.imhotep.comparador.EstoqueDispensacaoComparador;
+import br.com.imhotep.comparador.EstoqueDispensacaoValidadeComparador;
 import br.com.imhotep.comparador.SolicitacaoMedicamentoUnidadeItemComparador;
+import br.com.imhotep.consulta.raiz.DispensacaoSimplesConsulaRaiz;
 import br.com.imhotep.consulta.raiz.EstoqueConsultaRaiz;
 import br.com.imhotep.controle.ControleEstoque;
 import br.com.imhotep.entidade.DispensacaoSimples;
@@ -30,6 +30,7 @@ import br.com.imhotep.excecoes.ExcecaoDispensacaoSolicitacaoItemPendente;
 import br.com.imhotep.excecoes.ExcecaoEstoqueVazio;
 import br.com.imhotep.excecoes.ExcecaoProfissionalLogado;
 import br.com.imhotep.excecoes.ExcecaoQuantidadeAcimaEstoqueSolicitacaoUnidade;
+import br.com.imhotep.excecoes.ExcecaoReservaVazia;
 import br.com.imhotep.excecoes.ExcecaoSaldoInsuficienteEstoque;
 import br.com.imhotep.excecoes.ExcecaoSolicitacaoItemInseridaDuasVezes;
 import br.com.imhotep.excecoes.ExcecaoUnidadeAtual;
@@ -43,10 +44,46 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 	private List<EstoqueDispensacao> estoquesDispensacao = new ArrayList<EstoqueDispensacao>();
 	private List<SolicitacaoMedicamentoUnidadeItem> itens = new ArrayList<SolicitacaoMedicamentoUnidadeItem>();
 	private EstoqueDispensacao estoqueDispensacaoEdicao=null;
-	private Map<SolicitacaoMedicamentoUnidadeItem, List<EstoqueDispensacao>> itensLiberados = new HashMap<SolicitacaoMedicamentoUnidadeItem, List<EstoqueDispensacao>>();
+	private Map<SolicitacaoMedicamentoUnidadeItem, Set<EstoqueDispensacao>> itensLiberados = new HashMap<SolicitacaoMedicamentoUnidadeItem, Set<EstoqueDispensacao>>();
 	private Boolean quantidadeDispensadaDiferenteSolicitada;
 	private Boolean itemRecusado;
 	private String justificativaRecusa;
+	private boolean exibirDialogAlterarQuantidade = false;
+	private Integer quantidadeAlterada;
+	
+	public void alterarStatusAlterarQuantidade(){
+		setQuantidadeAlterada(null);
+		setExibirDialogAlterarQuantidade(!getExibirDialogAlterarQuantidade());
+	}
+	
+	public void cancelarAjusteQuantidade(){
+		alterarStatusAlterarQuantidade();
+		super.novaInstancia();
+	}
+	
+	public void alterarQuantidade(){
+		Integer qtdOriginal = getInstancia().getQuantidadeSolicitada();
+		boolean sucesso = false;
+		try {
+			getInstancia().setQuantidadeSolicitada(getQuantidadeAlterada());
+			validarQuantidade(getInstancia());
+			getInstancia().setQuantidadeSolicitada(getQuantidadeAlterada());
+			super.atualizar();
+			sucesso = true;
+			setExibirDialogAlterarQuantidade(false);
+			super.novaInstancia();
+		} catch (ExcecaoEstoqueVazio e) {
+			e.printStackTrace();
+		} catch (ExcecaoSaldoInsuficienteEstoque e) {
+			e.printStackTrace();
+		} catch (ExcecaoReservaVazia e) {
+			e.printStackTrace();
+		} finally{
+			if(!sucesso){
+				getInstancia().setQuantidadeSolicitada(qtdOriginal);
+			}
+		}
+	}
 	
 	public SolicitacaoMedicamentoUnidadeItemRaiz(){
 		super();
@@ -95,7 +132,11 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 	public void cancelarJustificativa(){
 		getInstancia().setJustificativa(null);
 		setQuantidadeDispensadaDiferenteSolicitada(false);
-		getInstancia().setQuantidadeDipensada(0);
+	}
+	
+	public Integer quantidadeSolicitadaItem(SolicitacaoMedicamentoUnidadeItem item){
+		Set<SolicitacaoMedicamentoUnidadeItem> keys = itensLiberados.keySet();
+		return keys.iterator().next().getQuantidadeSolicitada();
 	}
 	
 	public Integer somaTotalDispensadaItem(SolicitacaoMedicamentoUnidadeItem item){
@@ -103,7 +144,7 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 		Integer total = 0; 
 		for(SolicitacaoMedicamentoUnidadeItem key : keys){
 			if(key.equals(item)){
-				List<EstoqueDispensacao> edList = itensLiberados.get(key);
+				Set<EstoqueDispensacao> edList = itensLiberados.get(key);
 				for(EstoqueDispensacao ed : edList){
 					total += ed.getQuantidadeDispensada();
 				}
@@ -123,8 +164,7 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 	public void cancelarItemLiberado(){
 		estoquesDispensacao = new ArrayList<EstoqueDispensacao>();
 		estoqueDispensacaoEdicao = null;
-		itensLiberados = new HashMap<SolicitacaoMedicamentoUnidadeItem, List<EstoqueDispensacao>>();
-		super.novaInstancia();
+ 		super.novaInstancia();
 	}
 	
 	public void posFechamentoItemLiberado(){
@@ -144,12 +184,11 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 		}
 	}
 	
-	@Override
-	public void novaInstancia() {
+	public void limparInstancia() {
 		estoquesDispensacao = new ArrayList<EstoqueDispensacao>();
 		itens = new ArrayList<SolicitacaoMedicamentoUnidadeItem>();
 		estoqueDispensacaoEdicao=null;
-		itensLiberados = new HashMap<SolicitacaoMedicamentoUnidadeItem, List<EstoqueDispensacao>>();
+		itensLiberados = new HashMap<SolicitacaoMedicamentoUnidadeItem, Set<EstoqueDispensacao>>();
 		quantidadeDispensadaDiferenteSolicitada = false;
 		itemRecusado = false;
 		justificativaRecusa = null;
@@ -166,7 +205,7 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 	}
 	
 	private void fecharItemLiberado(){
-		List<EstoqueDispensacao> lotesDispensados = new ArrayList<EstoqueDispensacao>();
+		Set<EstoqueDispensacao> lotesDispensados = new HashSet<EstoqueDispensacao>();
 		for(EstoqueDispensacao ed : estoquesDispensacao){
 			if(ed.getQuantidadeDispensada() > 0){
 				lotesDispensados.add(ed);
@@ -186,7 +225,6 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 				}
 			}
 		}
-		
 		itensLiberados.put(getInstancia(), lotesDispensados);
 		getItens().remove(getInstancia());
 		getItens().add(getInstancia());
@@ -241,7 +279,7 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 	public void fecharSolicitacaoItens() throws ExcecaoDispensacaoSolicitacaoItemPendente{
 		for(SolicitacaoMedicamentoUnidadeItem item : getItens()){
 			atualizarItem(item);
-			List<EstoqueDispensacao> estoquesDispensacao = getItensLiberados().get(item);
+			Set<EstoqueDispensacao> estoquesDispensacao = getItensLiberados().get(item);
 			gerarMovimento(item, estoquesDispensacao);
 		}
 	}
@@ -252,7 +290,7 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 		smuir.atualizar();
 	}
 
-	private void gerarMovimento(SolicitacaoMedicamentoUnidadeItem item, List<EstoqueDispensacao> estoquesDispensacao) {
+	private void gerarMovimento(SolicitacaoMedicamentoUnidadeItem item, Set<EstoqueDispensacao> estoquesDispensacao) {
 		if(estoquesDispensacao == null)
 			return;
 		
@@ -282,40 +320,48 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 		}
 	}
 	
-	public void onCellEdit(CellEditEvent event) {  
-		Integer oldValue = Integer.valueOf(String.valueOf(event.getOldValue()));  
-		Integer newValue = Integer.valueOf(String.valueOf(event.getNewValue()));  
-          
-        if(newValue != null && !newValue.equals(oldValue)) {
-        		int index = event.getRowIndex();
-				EstoqueDispensacao ed = getEstoquesDispensacao().get(index);
-    			ed.setQuantidadeDispensada(newValue);
-    			getEstoquesDispensacao().remove(ed);
-    			getEstoquesDispensacao().add(ed);
-    			super.mensagem("Quantidade Alterada", "Anterior: " + oldValue + ", Atual:" + newValue, Constantes.INFO);
-        }  
-    }  
+	public void montarSugestao(){
+		List<SolicitacaoMedicamentoUnidadeItem> lista = new ArrayList<SolicitacaoMedicamentoUnidadeItem>(getItens());
+		for(SolicitacaoMedicamentoUnidadeItem smi : lista){
+			setInstancia(smi);
+			iniciarDispensacao();
+			fecharItemLiberado();
+			super.novaInstancia();
+		}
+	}
+	
+	public void carregarEstoquesDispensacao(SolicitacaoMedicamentoUnidadeItem linha){
+		getEstoquesDispensacao().addAll(getItensLiberados().get(linha));
+		List<Estoque> estoques = new EstoqueConsultaRaiz().consultarEstoquesMaterial(getInstancia().getMaterial());
+		for(Estoque estoque : estoques){
+			boolean achou = false;
+			for(EstoqueDispensacao ed : getEstoquesDispensacao()){
+				if(ed.getEstoque().equals(estoque)){
+					achou = true;
+					break;
+				}
+			}
+			if(!achou)
+				getEstoquesDispensacao().add(new EstoqueDispensacao(estoque, 0, getInstancia().getQuantidadeSolicitada()));
+		}
+	}
 	
 	public void iniciarDispensacao(){
-		if(itensLiberados.containsKey(getInstancia())){
-			getEstoquesDispensacao().addAll(itensLiberados.get(getInstancia()));
-		}else{
-			List<Estoque> estoques = new EstoqueConsultaRaiz().consultarEstoquesMaterial(getInstancia().getMaterial());
-			setEstoquesDispensacao(new ArrayList<EstoqueDispensacao>());
-			int quantidadeRestante = getInstancia().getQuantidadeSolicitada();
-			for(Estoque estoque : estoques){
-				if(quantidadeRestante == 0){
-					getEstoquesDispensacao().add(new EstoqueDispensacao(estoque, 0, getInstancia().getQuantidadeSolicitada()));
+		List<Estoque> estoques = new EstoqueConsultaRaiz().consultarEstoquesMaterial(getInstancia().getMaterial());
+		setEstoquesDispensacao(new ArrayList<EstoqueDispensacao>());
+		int quantidadeRestante = getInstancia().getQuantidadeSolicitada();
+		for(Estoque estoque : estoques){
+			if(quantidadeRestante == 0){
+				getEstoquesDispensacao().add(new EstoqueDispensacao(estoque, 0, getInstancia().getQuantidadeSolicitada()));
+			}else{
+				int quantidadeAtual = estoque.getQuantidadeAtual();
+				if((quantidadeAtual - quantidadeRestante) < 0){
+					getEstoquesDispensacao().add(new EstoqueDispensacao(estoque, quantidadeAtual, getInstancia().getQuantidadeSolicitada()));
+					quantidadeRestante = quantidadeRestante - quantidadeAtual;
 				}else{
-					int quantidadeAtual = estoque.getQuantidadeAtual();
-					if((quantidadeAtual - quantidadeRestante) < 0){
-						getEstoquesDispensacao().add(new EstoqueDispensacao(estoque, quantidadeAtual, getInstancia().getQuantidadeSolicitada()));
-						quantidadeRestante = quantidadeRestante - quantidadeAtual;
-					}else{
-						if((quantidadeAtual - quantidadeRestante) >= 0){
-							getEstoquesDispensacao().add(new EstoqueDispensacao(estoque, quantidadeRestante, getInstancia().getQuantidadeSolicitada()));
-							quantidadeRestante = 0;
-						}
+					if((quantidadeAtual - quantidadeRestante) >= 0){
+						getEstoquesDispensacao().add(new EstoqueDispensacao(estoque, quantidadeRestante, getInstancia().getQuantidadeSolicitada()));
+						quantidadeRestante = 0;
 					}
 				}
 			}
@@ -323,7 +369,8 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 	}
 	
 	public void dispensar(){
-		if(getInstancia().getQuantidadeDipensada() != getInstancia().getQuantidadeSolicitada() && (getInstancia().getJustificativa() == null || getInstancia().getJustificativa().isEmpty())){
+		Integer qtdDispensado = somaTotalDispensadaItem(getInstancia());
+		if(qtdDispensado != getInstancia().getQuantidadeSolicitada() && (getInstancia().getJustificativa() == null || getInstancia().getJustificativa().isEmpty())){
 			super.mensagem("Justifique o motivo da quantidade dispensada ser diferente da quantidade solicitada", "Preencha a justificativa", Constantes.FATAL);
 		}else{
 			try {
@@ -338,21 +385,22 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 		}
 	}
 	
-	private void validarQuantidade(SolicitacaoMedicamentoUnidadeItem instancia) throws ExcecaoEstoqueVazio, ExcecaoSaldoInsuficienteEstoque {
+	private void validarQuantidade(SolicitacaoMedicamentoUnidadeItem instancia) throws ExcecaoEstoqueVazio, ExcecaoSaldoInsuficienteEstoque, ExcecaoReservaVazia {
 		ControleEstoque ce = new ControleEstoque();
 		ce.liberarReserva(instancia.getQuantidadeSolicitada(), getInstancia().getMaterial());
 	}
 	
-	private void verificaItemInseridoDuasVezes() throws ExcecaoEstoqueVazio, ExcecaoSaldoInsuficienteEstoque, ExcecaoSolicitacaoItemInseridaDuasVezes{
+	private void verificaItemInseridoDuasVezes() throws ExcecaoEstoqueVazio, ExcecaoSaldoInsuficienteEstoque, ExcecaoSolicitacaoItemInseridaDuasVezes, ExcecaoReservaVazia{
 		for(SolicitacaoMedicamentoUnidadeItem item : getItens()){
 			if(item.getMaterial().equals(getInstancia().getMaterial())){
 				Integer total = item.getQuantidadeSolicitada();
 				total += getInstancia().getQuantidadeSolicitada();
+				validarQuantidade(new SolicitacaoMedicamentoUnidadeItem(total, item.getMaterial()));
 				item.setQuantidadeSolicitada(total);
-				validarQuantidade(item);
 				if(new SolicitacaoMedicamentoUnidadeItemRaiz(item).atualizar()){
 					getItens().remove(item);
 					getItens().add(item);
+					super.novaInstancia();
 					throw new ExcecaoSolicitacaoItemInseridaDuasVezes();
 				}
 			}
@@ -380,6 +428,8 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 		} catch (ExcecaoProfissionalLogado e) {
 			e.printStackTrace();
 		} catch (ExcecaoSolicitacaoItemInseridaDuasVezes e) {
+			e.printStackTrace();
+		} catch (ExcecaoReservaVazia e) {
 			e.printStackTrace();
 		} 
 		return false;
@@ -410,8 +460,15 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 		return false;
 	}
 	
+	public List<EstoqueDispensacao> listaEstoqueDispensacao(SolicitacaoMedicamentoUnidadeItem linha){
+		Set<EstoqueDispensacao> itens = itensLiberados.get(linha);
+		if(itens != null)
+			return new ArrayList<EstoqueDispensacao>(itens);
+		return new ArrayList<EstoqueDispensacao>();
+	}
+	
 	public List<EstoqueDispensacao> getEstoquesDispensacao() {
-		Collections.sort(estoquesDispensacao, new EstoqueDispensacaoComparador());
+		Collections.sort(estoquesDispensacao, new EstoqueDispensacaoValidadeComparador());
 		return estoquesDispensacao;
 	}
 
@@ -436,11 +493,18 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 		this.estoqueDispensacaoEdicao = estoqueDispensacaoEdicao;
 	}
 
-	public Map<SolicitacaoMedicamentoUnidadeItem, List<EstoqueDispensacao>> getItensLiberados() {
+	public Map<SolicitacaoMedicamentoUnidadeItem, Set<EstoqueDispensacao>> getItensLiberados() {
 		return itensLiberados;
 	}
 
-	public void setItensLiberados(Map<SolicitacaoMedicamentoUnidadeItem, List<EstoqueDispensacao>> itensLiberados) {
+	public List<EstoqueDispensacao> pegarEstoqueItensLiberados(SolicitacaoMedicamentoUnidadeItem linha) {
+		Set<EstoqueDispensacao> set = getItensLiberados().get(linha);
+		if(set != null && !set.isEmpty())
+			return new ArrayList<EstoqueDispensacao>(set);
+		return new ArrayList<EstoqueDispensacao>();
+	}
+	
+	public void setItensLiberados(Map<SolicitacaoMedicamentoUnidadeItem, Set<EstoqueDispensacao>> itensLiberados) {
 		this.itensLiberados = itensLiberados;
 	}
 
@@ -467,6 +531,35 @@ public class SolicitacaoMedicamentoUnidadeItemRaiz extends PadraoHome<Solicitaca
 
 	public void setJustificativaRecusa(String justificativaRecusa) {
 		this.justificativaRecusa = justificativaRecusa;
+	}
+
+	public void montarImpressao() {
+		List<SolicitacaoMedicamentoUnidadeItem> lista = new ArrayList<SolicitacaoMedicamentoUnidadeItem>(getItens());
+		for(SolicitacaoMedicamentoUnidadeItem smi : lista){
+			List<DispensacaoSimples> dispensacoesSolicitacaoItem = new DispensacaoSimplesConsulaRaiz().consultarDispensacoesSolicitacaoItem(smi);
+			Set<EstoqueDispensacao> setED = new HashSet<EstoqueDispensacao>();
+			for(DispensacaoSimples e : dispensacoesSolicitacaoItem){
+				setED.add(new EstoqueDispensacao(e.getMovimentoLivro().getEstoque(), e.getMovimentoLivro().getQuantidadeMovimentacao(), smi.getQuantidadeSolicitada()));
+			}
+			getItensLiberados().put(smi, setED);
+		}
+	}
+
+	public boolean getExibirDialogAlterarQuantidade() {
+		return exibirDialogAlterarQuantidade;
+	}
+
+	public void setExibirDialogAlterarQuantidade(
+			boolean exibirDialogAlterarQuantidade) {
+		this.exibirDialogAlterarQuantidade = exibirDialogAlterarQuantidade;
+	}
+
+	public Integer getQuantidadeAlterada() {
+		return quantidadeAlterada;
+	}
+
+	public void setQuantidadeAlterada(Integer quantidadeAlterada) {
+		this.quantidadeAlterada = quantidadeAlterada;
 	}
 	
 }
