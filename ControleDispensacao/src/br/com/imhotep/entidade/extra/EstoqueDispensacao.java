da14716cbcae8caf612d3cb048bb396fc1d9ba8f
marlonassis <marlonassis@gmail.com>
2013-08-24 12:14:55 -0300

    Criado o hash e o equals da classe.

--- ControleDispensacao/src/br/com/imhotep/entidade/extra/EstoqueDispensacao.java
+++ ControleDispensacao/src/br/com/imhotep/entidade/extra/EstoqueDispensacao.java
@@ -36,4 +36,50 @@ public class EstoqueDispensacao {
 	public void setQuantidadeSolicitada(Integer quantidadeSolicitada) {
 		this.quantidadeSolicitada = quantidadeSolicitada;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((estoque == null) ? 0 : estoque.hashCode());
+		result = prime
+				* result
+				+ ((quantidadeDispensada == null) ? 0 : quantidadeDispensada
+						.hashCode());
+		result = prime
+				* result
+				+ ((quantidadeSolicitada == null) ? 0 : quantidadeSolicitada
+						.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EstoqueDispensacao other = (EstoqueDispensacao) obj;
+		if (estoque == null) {
+			if (other.estoque != null)
+				return false;
+		} else if (!estoque.equals(other.estoque))
+			return false;
+		if (quantidadeDispensada == null) {
+			if (other.quantidadeDispensada != null)
+				return false;
+		} else if (!quantidadeDispensada.equals(other.quantidadeDispensada))
+			return false;
+		if (quantidadeSolicitada == null) {
+			if (other.quantidadeSolicitada != null)
+				return false;
+		} else if (!quantidadeSolicitada.equals(other.quantidadeSolicitada))
+			return false;
+		return true;
+	}
+	
+	
+	
 }
