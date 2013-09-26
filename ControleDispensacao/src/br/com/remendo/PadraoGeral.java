69910d408efa9a048b7a8e333b2594d2433af2e8
marlonassis <marlonassis@gmail.com>
2013-09-25 22:54:30 -0300

    Adicionado método para exibir mensagens.

--- ControleDispensacao/src/br/com/remendo/PadraoGeral.java
+++ ControleDispensacao/src/br/com/remendo/PadraoGeral.java
@@ -1,9 +1,15 @@
 package br.com.remendo;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.context.FacesContext;
+
 import br.com.remendo.gerenciador.GerenciadorConexao;
 
 public class PadraoGeral extends GerenciadorConexao {
 
-
+	protected void mensagem(String msg, String msg2, Severity tipoMensagem){
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(tipoMensagem,msg, msg2));
+	}
 	
 }
