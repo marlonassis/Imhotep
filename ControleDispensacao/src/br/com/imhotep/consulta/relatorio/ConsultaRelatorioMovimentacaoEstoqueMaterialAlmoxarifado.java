6c60b4cea85670028ca082bae744dcd5ed84141e
marlonassis <marlonassis@gmail.com>
2014-09-08 16:30:09 -0300

    Criado relatórios.

---
+++ ControleDispensacao/src/br/com/imhotep/consulta/relatorio/ConsultaRelatorioMovimentacaoEstoqueMaterialAlmoxarifado.java
@@ -0,0 +1,69 @@
+package br.com.imhotep.consulta.relatorio;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import br.com.imhotep.auxiliar.Constantes;
+import br.com.imhotep.entidade.MaterialAlmoxarifado;
+import br.com.imhotep.entidade.TipoMovimentoAlmoxarifado;
+import br.com.imhotep.entidade.Unidade;
+import br.com.imhotep.entidade.relatorio.MovimentacaoEstoqueMaterialAlmoxarifado;
+import br.com.imhotep.enums.TipoOperacaoEnum;
+import br.com.remendo.ConsultaGeral;
+
+public class ConsultaRelatorioMovimentacaoEstoqueMaterialAlmoxarifado extends ConsultaGeral<Object[]> {
+	
+	public List<MovimentacaoEstoqueMaterialAlmoxarifado> consultarResultados(MaterialAlmoxarifado materialAlmoxarifado, Date dataIni, Date dataFim, Unidade unidade, TipoMovimentoAlmoxarifado tipoMovimentoAlmoxarifado, TipoOperacaoEnum tipoOperacao, boolean agruparPorLote){
+		return consultaMovimentoPeriodo(materialAlmoxarifado, dataIni, dataFim, unidade, tipoMovimentoAlmoxarifado, tipoOperacao, agruparPorLote);
+	}
+	
+	private List<MovimentacaoEstoqueMaterialAlmoxarifado> consultaMovimentoPeriodo(MaterialAlmoxarifado materialAlmoxarifado, Date dataIni, Date dataFim, Unidade unidade, TipoMovimentoAlmoxarifado tipoMovimentoAlmoxarifado, TipoOperacaoEnum tipoOperacao, boolean agruparPorLote) {
+		ConsultaGeral<MovimentacaoEstoqueMaterialAlmoxarifado> cg = new ConsultaGeral<MovimentacaoEstoqueMaterialAlmoxarifado>();
+		HashMap<Object, Object> map = new HashMap<Object, Object>();
+		map.put("dataIni", dataIni);
+		map.put("dataFim", dataFim);
+		String sql = "select new br.com.imhotep.entidade.relatorio.MovimentacaoEstoqueMaterialAlmoxarifado(o.tipoMovimentoAlmoxarifado, " +
+				"a.nome, o.estoqueAlmoxarifado.lote, o.quantidadeMovimentacao, o.quantidadeAtual, o.dataMovimento, " +
+				"b, o.estoqueAlmoxarifado.materialAlmoxarifado.descricao, o.justificativa) from MovimentoLivroAlmoxarifado o " +
+				"join o.profissionalInsercao b "+
+				"LEFT OUTER JOIN o.dispensacaoSimplesAlmoxarifado.unidadeDispensada a "+
+				"where o.dataMovimento >= :dataIni and o.dataMovimento <= :dataFim";
+		
+		if(unidade != null){
+			sql += " and a.idUnidade = :idUnidade ";
+			map.put("idUnidade", unidade.getIdUnidade());
+		}
+		
+		if(materialAlmoxarifado != null){
+			sql += " and o.estoqueAlmoxarifado.materialAlmoxarifado.idMaterialAlmoxarifado = :idMaterialAlmoxarifado ";
+			map.put("idMaterialAlmoxarifado", materialAlmoxarifado.getIdMaterialAlmoxarifado());
+		}
+		
+		if(tipoMovimentoAlmoxarifado != null && tipoMovimentoAlmoxarifado.getIdTipoMovimentoAlmoxarifado() != 0){
+			sql += " and o.tipoMovimentoAlmoxarifado.idTipoMovimentoAlmoxarifado = :idTipoMovimentoAlmoxarifado";
+			map.put("idTipoMovimentoAlmoxarifado", tipoMovimentoAlmoxarifado.getIdTipoMovimentoAlmoxarifado());
+		}
+		
+		if(tipoOperacao != null){
+			sql += " and o.tipoMovimentoAlmoxarifado.tipoOperacao = :tipoOperacao";
+			map.put("tipoOperacao", tipoOperacao);
+		}
+		
+		if(agruparPorLote){
+			sql += " order by o.estoqueAlmoxarifado.lote, o.dataMovimento asc";
+		}else{
+			sql += " order by o.dataMovimento asc";
+		}
+		
+		ArrayList<MovimentacaoEstoqueMaterialAlmoxarifado> list = new ArrayList<MovimentacaoEstoqueMaterialAlmoxarifado>(cg.consulta(new StringBuilder(sql), map));
+		
+		if(list == null || list.isEmpty()){
+			list.add(new MovimentacaoEstoqueMaterialAlmoxarifado(Constantes.MENSAGEM_RELATORIO_VAZIO));
+		}
+		
+		return list;
+	}
+	
+}
---
+++ ControleDispensacao/src/br/com/imhotep/consulta/relatorio/RelatorioNotaFiscalAlmoxarifado.java
@@ -0,0 +1,86 @@
+package br.com.imhotep.consulta.relatorio;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import br.com.imhotep.entidade.extra.FinanceiroGrupoAlmoxarifado;
+import br.com.imhotep.entidade.extra.FinanceiroGrupoNotaFiscalAlmoxarifado;
+import br.com.imhotep.entidade.extra.FinanceiroNotaFiscalAlmoxarifadoDesconto;
+import br.com.imhotep.linhaMecanica.LinhaMecanica;
+
+public class RelatorioNotaFiscalAlmoxarifado {
+	
+	public static void main(String[] args) {
+		List<FinanceiroGrupoAlmoxarifado> lista = new RelatorioNotaFiscalAlmoxarifado().consultarResultados(new Date());
+		System.out.println(lista);
+	}
+	
+	public List<FinanceiroNotaFiscalAlmoxarifadoDesconto> listaNFDesconto(Date dataIni){
+		String data = new SimpleDateFormat("yyyy-MM").format(dataIni);
+		String sqlDescontoNFs = "select  a.cv_identificacao notaFiscal, coalesce(a.db_valor_desconto, 0) valorDesconto from tb_nota_fiscal_almoxarifado a "+ 
+								"where to_char(a.dt_data_contabil, 'YYYY-MM') = '"+data+"' and a.bl_doacao is false and a.db_valor_desconto > 0 "+
+								"order by a.cv_identificacao ";
+		List<FinanceiroNotaFiscalAlmoxarifadoDesconto> lista = new ArrayList<FinanceiroNotaFiscalAlmoxarifadoDesconto>();
+		LinhaMecanica lm = new LinhaMecanica("db_imhotep", "127.0.0.1");
+		ResultSet rs = lm.consultar(sqlDescontoNFs);
+		try {
+			while(rs.next()){
+				String notaFiscal = rs.getString("notaFiscal");
+				double desconto = rs.getDouble("valorDesconto");
+				lista.add(new FinanceiroNotaFiscalAlmoxarifadoDesconto(notaFiscal, desconto));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return lista;
+	}
+
+	public List<FinanceiroGrupoAlmoxarifado> consultarResultados(Date dataIni){
+		return consultaMovimentoPeriodo(dataIni);
+	}
+	
+	private List<FinanceiroGrupoAlmoxarifado> consultaMovimentoPeriodo(Date dataIni) {
+		String data = new SimpleDateFormat("yyyy-MM").format(dataIni);
+		String sql = "select  a.cv_descricao grupo, e.cv_identificacao notaFiscal, f.cv_razao_social fornecedor, "+
+				 	"f.cv_cadastro cadastro, sum((d.db_valor_unitario * d.in_quantidade_entrada)) total from tb_grupo_almoxarifado a "+ 
+					"inner join tb_material_almoxarifado b on b.id_grupo_almoxarifado = a.id_grupo_almoxarifado "+
+					"inner join tb_estoque_almoxarifado c on c.id_material_almoxarifado = b.id_material_almoxarifado "+
+					"inner join tb_nota_fiscal_estoque_almoxarifado d on d.id_estoque_almoxarifado = c.id_estoque_almoxarifado "+
+					"inner join tb_nota_fiscal_almoxarifado e on e.id_nota_fiscal_almoxarifado = d.id_nota_fiscal_almoxarifado "+
+					"inner join tb_fornecedor f on f.id_fornecedor = e.id_fornecedor "+
+					"where to_char(e.dt_data_contabil, 'YYYY-MM') = '"+data+"' and a.bl_sem_financeiro is false "+
+					"group by grupo, notaFiscal, fornecedor, cadastro "+
+					"order by grupo, e.cv_identificacao";
+		
+		List<FinanceiroGrupoAlmoxarifado> listGrupo = new ArrayList<FinanceiroGrupoAlmoxarifado>();
+		List<FinanceiroGrupoNotaFiscalAlmoxarifado> listItens = new ArrayList<FinanceiroGrupoNotaFiscalAlmoxarifado>();
+		String grupoTemp="";
+		LinhaMecanica lm = new LinhaMecanica("db_imhotep", "127.0.0.1");
+		for(Object[] obj : lm.getListaResultado(sql)){
+			String grupo = String.valueOf(obj[0]);
+			String notaFiscal = String.valueOf(obj[1]);
+			String fornecedor = String.valueOf(obj[2]);
+			String inscricao = String.valueOf(obj[3]);
+			Double total = Double.valueOf(String.valueOf(obj[4]));
+			
+			if(grupoTemp.equals("")){
+				grupoTemp = grupo;
+			}else
+				if(!grupoTemp.equals(grupo)){
+					listGrupo.add(new FinanceiroGrupoAlmoxarifado(grupoTemp, listItens));
+					listItens = new ArrayList<FinanceiroGrupoNotaFiscalAlmoxarifado>();
+					grupoTemp = grupo;
+				}
+			
+			listItens.add(new FinanceiroGrupoNotaFiscalAlmoxarifado(notaFiscal, fornecedor, inscricao, total));
+		}
+		if(!grupoTemp.equals(""))
+			listGrupo.add(new FinanceiroGrupoAlmoxarifado(grupoTemp, listItens));
+		return listGrupo;
+	}
+	
+}
