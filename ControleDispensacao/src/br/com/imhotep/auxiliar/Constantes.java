1c87b65c781d899d4fbc79703111c7dfe07e21bf
marlonassis <marlonassis@gmail.com>
2014-09-08 16:04:49 -0300

    Adicionado algumas constantes e ajustado os caracteres que ficaram desconfigurados.

--- ControleDispensacao/src/br/com/imhotep/auxiliar/Constantes.java
+++ ControleDispensacao/src/br/com/imhotep/auxiliar/Constantes.java
@@ -18,11 +18,15 @@ public class Constantes {
 	public static final String PAGINA_MANUTENCAO = "/imhotep/PaginasWeb/manutencao.hu";
 	public static final String PAGINA_RECUSA_IEXPLORER = "/imhotep/PaginasWeb/recusaIExplorer.hu";
 	public static final String PAGINA_RECUPERACAO_SENHA = "/imhotep/PaginasWeb/Publico/recuperarSenha.hu";
+	public static final String PAGINA_SOLICITACAO_MATERIAL = "/imhotep/PaginasWeb/Solicitacoes/SolicitacaoMaterialAlmoxarifadoUnidades/solicitacaoMaterialAlmoxarifadoUnidades.hu";
+	public static final String PAGINA_SOLICITACAO_MEDICAMENTO = "/imhotep/PaginasWeb/Solicitacoes/SolicitacaoMedicamentoUnidade/solicitacaoMedicamentoUnidade.hu";
+	public static final String PAGINA_DEVOLUCAO_MATERIAL = "/imhotep/PaginasWeb/Almoxarifado/DevolucaoMaterial/devolucaoMaterial.hu";
+	public static final String PAGINA_DEVOLUCAO_MEDICAMENTO = "/imhotep/PaginasWeb/Solicitacoes/DevolucaoMedicamento/devolucaoMedicamento.hu";
 	
 	public static final String PAINEL_MEDICAMENTO_VENCIDO = "Medicamentos Vencidos";
-	public static final String PAINEL_SOLICITACOES_MEDICAMENTO_USUARIO = "Solicita√ß√µes de Medicamento pelo Usu√°rio";
+	public static final String PAINEL_SOLICITACOES_MEDICAMENTO_USUARIO = "Solicitações de Medicamento pelo Usuário";
 	
-	public static final String MATERIAL_ANTIBIOTICO = "ANTIBI√ìTICO";
+	public static final String MATERIAL_ANTIBIOTICO = "ANTIBIÓTICO";
 	
 	public static final String PRESCRICAO_PACIENTE_TAB = "prescricaoPacienteTab";
 	public static final String PRESCRICAO_FARMACOLOGICA_TAB = "prescricaoFarmacologicaTab";
@@ -33,12 +37,22 @@ public class Constantes {
 	public static final String RELATORIO_CUSTO_ESTOQUE_PATH = "/WEB-INF/classes/br/com/imhotep/relatorio/RelatorioCustoEstoque.jasper";
 	
 	public static final String URL_BANCO = "jdbc:postgresql://127.0.0.1:5432/db_imhotep";
-	public static final String NOME_BANCO = "db_imhotep";
+	public static final String NOME_BANCO_IMHOTEP = "db_imhotep";
 	public static final String USUARIO_BANCO = "imhotep";
 	public static final String SENHA_BANCO = "##Imhotep09*123789!@#*()";
 	
 	public static final String IP_HU = "200.133.41.5";
+	public static final String IP_IMHOTEP_REMOTO = "200.133.41.8";
+	public static final String IP_IMHOTEP_LINHA_MECANICA = "127.0.0.1";
 	public static final String IP_LOCAL = "127.0.0.1";
+	
+	public static final String NOME_BANCO_AGHU = "dbaghu";
+	public static final String USUARIO_BANCO_AGHU = "ugen_integra";
+	public static final String SENHA_BANCO_AGHU = "aghuintegracao";
+	public static final String PORTA_BANCO_AGHU = "6544";
+	public static final String IP_AGHU = "192.168.20.12";
+	
+	public static final String NOME_ROOT_ORGANIZACAO = "EBSERH";
 	
 	public static final String MENSAGEM_RELATORIO_VAZIO = "Nenhum registro encontrado.";
 	
@@ -50,5 +64,19 @@ public class Constantes {
 	public static final Locale LOCALE_BRASIL = new Locale ("pt", "BR");
 	
 	public static final String JUSTIFICATIVA_ENTRADA_SEM_NOTA = "Entrada sem nota fiscal";
+	public static final String JUSTIFICATIVA_RECUSA_SOLICITACAO_ITEM = "Solicitação Recusada";
 	
+	public static final Integer QUANTIDADE_BLOQUEIO_USUARIO = 5;
+	public static final String SENHA_PADRAO = "123456";
+	
+	public static final int ID_TIPO_MOVIMENTO_SAIDA_AJUSTE_SAIDA_ALMOXARIFADO = 4;
+	public static final int ID_TIPO_MOVIMENTO_SAIDA_AJUSTE_PERDA_ALMOXARIFADO = 5;
+	public static final int ID_TIPO_MOVIMENTO_SAIDA_DISPENSACAO_ALMOXARIFADO = 6;
+	
+	public static final int ID_TIPO_MOVIMENTO_SAIDA_AJUSTE_SAIDA_FARMACIA = 23;
+	public static final int ID_TIPO_MOVIMENTO_SAIDA_AJUSTE_PERDA_FARMACIA = 25;
+	public static final int ID_TIPO_MOVIMENTO_DEVOLUCAO_MEDICAMENTO = 22;
+	public static final int ID_TIPO_MOVIMENTO_SAIDA_DISPENSACAO_FARMACIA = 21;
+	
+	public static final int ID_UNIDADE_LABORATORIO = 18;
 }
