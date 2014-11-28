7efed5918cd121a658be8891592042376bb170de
Márlon Assis <marlonassis@gmail.com>
2014-11-28 17:42:09 -0300

    Corrigido a consulta dos médicos para incluir os que não possuem consultas realizadas.

--- ControleDispensacao/src/br/com/imhotep/consulta/autoComplete/AGHUProfissionalComprovanteAutoComplete.java
+++ ControleDispensacao/src/br/com/imhotep/consulta/autoComplete/AGHUProfissionalComprovanteAutoComplete.java
@@ -16,12 +16,9 @@ public class AGHUProfissionalComprovanteAutoComplete  extends ConsultaGeral<Stri
 
 	public Collection<String> autoComplete(String nome) {
 		nome = nome.trim();
-		String sql = "select profissional, especialidade, crm from agh.temp_compro_consul "+
-				"where lower(profissional) ilike lower('%"+nome+"%') "+
-				"group by profissional, especialidade, crm "
-				+ "order by profissional, especialidade, crm";
+		String sql = getSqlConsultaMedicoAGHU(nome);
 		LinhaMecanicaAGHU lma = new LinhaMecanicaAGHU();
-		List<Object[]> listaResultado = lma.getListaResultado(sql);
+		List<Object[]> listaResultado = lma.getListaResultado(sql, 3);
 		Collection<String> res = new ArrayList<String>();
 		for(Object[] o : listaResultado){
 			String item = String.valueOf(o[0]).concat(" / ").concat(String.valueOf(o[1]));
@@ -35,9 +32,28 @@ public class AGHUProfissionalComprovanteAutoComplete  extends ConsultaGeral<Stri
 			return res;
 		else{
 			List<String> l = new ArrayList<String>();
-			l.add("N‹o encontrado");
+			l.add("NÃ£o encontrado");
 			return l;
 		}
+	}
+	
+	private String getSqlConsultaMedicoAGHU(String nome){
+		String sql = "SELECT distinct pessoa.nome AS profissional, esp.nome_especialidade AS especialidade, qualificacao.nro_reg_conselho AS crm "+
+				   "FROM agh.aac_consultas con "+
+				   "JOIN agh.aac_grade_agendamen_consultas grade ON con.grd_seq = grade.seq "+
+				   "JOIN agh.agh_especialidades esp ON grade.esp_seq = esp.seq "+
+				   "JOIN agh.agh_equipes eqp ON grade.eqp_seq = eqp.seq "+
+				   "JOIN agh.aip_pacientes paciente ON paciente.codigo = con.pac_codigo "+
+				   "LEFT JOIN agh.aip_enderecos_pacientes endereco ON paciente.codigo = endereco.pac_codigo "+
+				   "LEFT JOIN agh.aip_bairros bairro ON bairro.codigo = endereco.bcl_bai_codigo "+
+				   "LEFT JOIN agh.aip_logradouros logradouro ON logradouro.codigo = endereco.bcl_clo_lgr_codigo "+
+				   "LEFT JOIN agh.aip_cidades cidade ON logradouro.cdd_codigo = cidade.codigo AND endereco.bcl_clo_lgr_codigo IS NOT NULL OR endereco.cdd_codigo = cidade.codigo AND endereco.cdd_codigo IS NOT NULL "+
+				   "LEFT JOIN agh.rap_servidores serv ON grade.pre_ser_matricula = serv.matricula AND grade.pre_ser_vin_codigo = serv.vin_codigo "+
+				   "LEFT JOIN agh.rap_pessoas_fisicas pessoa ON serv.pes_codigo = pessoa.codigo "+
+				   "LEFT JOIN agh.rap_qualificacoes qualificacao ON qualificacao.pes_codigo = pessoa.codigo "+
+				  "WHERE con.ret_seq in (9, 10) and pessoa.nome ilike '%"+nome+"%' "+
+				  "ORDER BY pessoa.nome";
+		return sql;
 	}
 	
 }
--- ControleDispensacao/src/br/com/imhotep/testes/Teste.java
+++ ControleDispensacao/src/br/com/imhotep/testes/Teste.java
@@ -1,32 +1,14 @@
 package br.com.imhotep.testes;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
-import org.primefaces.model.menu.DefaultMenuItem;
-import org.primefaces.model.menu.DefaultMenuModel;
-import org.primefaces.model.menu.DefaultSubMenu;
-import org.primefaces.model.menu.MenuModel;
+import br.com.imhotep.linhaMecanica.LinhaMecanicaAGHU;
 
 
 
 public class Teste {
-	public static void main(String[] args) {
-//		int v = 8 + r.nextInt(17); 
-		int max = 0;
-		for(int i = 0; i<1000;i++){
-			int a = new Random().nextInt(59);
-			if(a > max)
-				max = a;
-			System.out.println(a);
-		}
-		System.out.println(max);
-	}
+	
 
 }
